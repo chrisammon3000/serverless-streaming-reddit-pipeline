a9229e741c1b52a07764f380e6649d1108563d7c
Gregory Lindsey <gclindsey@gmail.com>
2020-10-21 18:05:38 -0700

    Add GlueScriptsBucket

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -82,7 +82,18 @@ S3_CONFIG_BUCKET=$(aws cloudformation describe-stacks \
 --output text)
 
 aws s3 cp ./config s3://$S3_CONFIG_BUCKET/config --recursive
-aws s3 cp ./glue-scripts s3://$S3_CONFIG_BUCKET/glue_scripts --recursive
+#aws s3 cp ./glue-scripts s3://$S3_CONFIG_BUCKET/glue_scripts --recursive
+
+# Sync glue scripts 
+echo
+echo -e "\e[38;5;0;48;5;255m******* Syncing Glue scripts... *******\e[0m"
+echo
+S3_SCRIPTS_BUCKET=$(aws cloudformation describe-stacks \
+--stack-name $STACK_NAME \
+--query "Stacks[0].Outputs[?OutputKey=='GlueScriptsBucketName'].OutputValue" \
+--output text)
+
+aws s3 cp ./glue-scripts s3://$S3_SCRIPTS_BUCKET/glue_scripts --recursive
 
 
 # Deploy Serverless if indicated
