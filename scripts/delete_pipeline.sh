2486caa68d52b130df322d865779757a5a045479
Gregory Lindsey <gclindsey@gmail.com>
2020-10-23 18:42:40 -0700

    Revert to version 1

--- scripts/delete_pipeline.sh
+++ scripts/delete_pipeline.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 source $(dirname "$0")/set_env.sh
-read -p "This will delete all resources and data. Continue? " -n 1 -r
+echo "This will delete all data and resources in stack:"
+echo $STACK_NAME
+read -p "Continue? " -n 1 -r
 echo && echo
 
 if [[ ! $REPLY =~ ^[Yy]$ ]]
@@ -10,12 +12,25 @@ then
     [[ "$0" = "$BASH_SOURCE" ]] && exit 0 || return 0
 fi
 
+# Delete stacks
+echo
+echo -e "\e[38;5;0;48;5;255m******* Deleting CloudFormation stacks... *******\e[0m"
+echo
+# Delete CloudFormation nested stack
+aws cloudformation delete-stack \
+--stack-name $STACK_NAME
+echo "Deleted stack: $STACK_NAME"
+
+echo
 echo -e "\e[38;5;0;48;5;255m******* Deleting Serverless resources... *******\e[0m"
+echo
 # Delete serverless resources
 cd src/functions/pipeline && \
 serverless remove
 
+echo
 echo -e "\e[38;5;0;48;5;255m******* Deleting S3 buckets and objects... *******\e[0m"
+echo
 # Get and delete objects on cfn bucket
 S3_CFN_BUCKET=$(aws cloudformation describe-stacks \
     --stack-name $STACK_NAME-s3 \
@@ -48,26 +63,20 @@ S3_SCRIPTS_BUCKET=$(aws cloudformation describe-stacks \
 aws s3 rm --recursive s3://$S3_SCRIPTS_BUCKET
 echo "Deleted S3 bucket: $S3_SCRIPTS_BUCKET"
 
-# Delete stacks
-echo -e "\e[38;5;0;48;5;255m******* Deleting CloudFormation stacks... *******\e[0m"
-
-# Delete CloudFormation nested stack
-aws cloudformation delete-stack \
---stack-name $STACK_NAME
-echo "Deleted stack: $STACK_NAME"
-
-# Delete S3 stack
-aws cloudformation delete-stack \
---stack-name $STACK_NAME-s3
-echo "Deleted stack: $STACK_NAME-s3"
-
 # Delete SSM Parameters
+echo
 echo -e "\e[38;5;0;48;5;255m******* Deleting SSM Parameters... *******\e[0m"
+echo
 aws ssm delete-parameter --name /$SSM_KEY/praw_cid
 aws ssm delete-parameter --name /$SSM_KEY/praw_secret
 aws ssm delete-parameter --name /$SSM_KEY/praw_useragent
 aws ssm delete-parameter --name /$SSM_KEY/praw_username
 aws ssm delete-parameter --name /$SSM_KEY/praw_password
+
+# Delete S3 stack
+aws cloudformation delete-stack \
+--stack-name $STACK_NAME-s3
+#echo "Deleted stack: $STACK_NAME-s3"
 
 echo "Finished"
 
