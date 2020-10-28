a2ec0da0d0aca328eafed8ef2b766e4a87afd46e
Gregory Lindsey <gclindsey@gmail.com>
2020-10-27 18:38:40 -0700

    Switch set_env

--- scripts/set_env.sh
+++ scripts/set_env.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-STAGE=dev
+STAGE=prod
 APP_NAME=reddit-pipeline
 APP_VERSION=1
 REGION=us-east-1
@@ -8,10 +8,11 @@ ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
 STACK_NAME=$STAGE-$APP_NAME-$APP_VERSION-$REGION
 
 # Reddit API Params
-CLIENT_ID=hYOoZaQK3ZsJqw
-CLIENT_SECRET=WenXYVT8isAzRDxddc8_e_B38Tc
+CLIENT_ID=<CLIENT_ID>
+CLIENT_SECRET=<USERAGENT>
 USERAGENT=ssrp
-USERNAME=abk7x4
-PASSWORD="ABK!g2r0e2g0"
+USERNAME=<USERNAME>
+PASSWORD=<PASSWORD>
 
 SSM_KEY=$STAGE-$APP_NAME/$APP_VERSION/$REGION
+
